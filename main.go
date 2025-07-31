561059ab66b2c1a5e3b9c09ad1ce7ed0be86630d
rootp1 <arnav.iitr@gmail.com>
2025-07-31 17:23:18 +0530

    genesis block

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
-	"log"
 	"bytes"
 	"strconv"
 	"time"
@@ -39,6 +38,14 @@ func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
 func main() { 
