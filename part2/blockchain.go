58a38b8dd4a17dd4338a64549cd751da904a2b42
rootp1 <arnav.iitr@gmail.com>
2025-08-04 03:02:52 +0530

    test

--- part2/blockchain.go
+++ part2/blockchain.go
@@ -129,5 +129,8 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
