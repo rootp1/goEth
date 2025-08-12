c9d7e2ead5ee1f15f708fc7272c74dcaceb1a224
rootp1 <arnav.iitr@gmail.com>
2025-08-13 03:51:48 +0530

    new AddBlock

--- part3/database.go
+++ part3/database.go
@@ -44,3 +44,25 @@ type Blockchain struct {
 	tip []byte
 	db  *bolt.DB
 }
+
+func (bc *Blockchain) AddBlock(data string) {
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		err = b.Put([]byte("l"), newBlock.Hash)
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
+}
