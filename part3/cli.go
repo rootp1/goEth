6290e08af3ac0c281e7b6c3bfacc0935d3dba7c2
rootp1 <arnav.iitr@gmail.com>
2025-08-15 07:50:19 +0530

    cli printchain

--- part3/cli.go
+++ part3/cli.go
@@ -1,5 +1,9 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strconv"
+)
 
 type CLI struct {
 	bc *Blockchain
@@ -10,3 +14,22 @@ func (cli *CLI) addBlock(data string) {
 	fmt.Println("block added")
 }
 
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
